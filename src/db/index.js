7289aa448f746a1f86ed63e0bf9184802d94674f
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2024-09-01 19:44:09 +0530

    cors and cookie-parser

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB  = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`\n MongoDB Connected to ${connectionInstance.connection.host}`);
+        console.log(`\nMongoDB Connected to ${connectionInstance.connection.host}`);
         
     } catch (error) {
         console.log("MongoDb Connection Error : ",error);
